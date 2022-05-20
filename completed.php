1b1127273854e0223dc5515efd272a1679cfcc65
Akshat Modani <akshatmodani@jklu.edu.in>
2022-05-20 11:33:43 +0530

    delete

--- completed.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include 'connection.php';
-$id = $_GET['id'];
-$sql3 = "UPDATE `appointment` SET Completed=1 WHERE ID='$id'";
-$result3 = mysqli_query($conn, $sql3) or die("Your query is not right");
-header("location:counsellordash.php");
-?>
--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$conn=mysqli_connect("localhost","root","","counselling") or die("The connection to the database has not been made");
-
-
-?>
